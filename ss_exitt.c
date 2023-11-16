033fb8dd2aa0e35b1cf428af7761e821f6edff13
calebdfk <dkweteza@oulook.com>
2023-11-15 22:09:33 -0800

    ss_exitt.c

--- ss_exitt.c
+++ ss_exitt.c
@@ -1,5 +1,13 @@
 #include "shell.h"
 
+/**
+ * exitt - Implementation of the 'exit' built-in command.
+ * @arv: Array of arguments.
+ *
+ * This function handles the 'exit' command, performing any necessary cleanup
+ * and exiting the program.
+ */
+
 void exitt(char **arv)
 
 {
